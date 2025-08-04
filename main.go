7ab16c5e0a78f9e1dcb09bff234bfa7946f6dcb9
ramZenit <a.mb@outlook.it>
2025-08-04 14:10:54 +0200

    new commands pkg

--- main.go
+++ main.go
@@ -6,17 +6,26 @@ import (
 	"github.com/ramZenit/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Println("Error reading the config file:", err)
 		return
 	}
-	cfg.SetUser("ramZenit")
-	newCfg, err := config.Read()
-	if err != nil {
-		fmt.Println("Error reading the config file:", err)
-		return
-	}
-	fmt.Printf("main: %v\n", newCfg)
+
+	fmt.Printf("main: %v\n", cfg)
+
 }
