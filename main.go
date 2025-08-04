bc3f8924dd85f9d4a297e412c703e4c467185b60
ramZenit <a.mb@outlook.it>
2025-08-04 17:50:09 +0200

    refactored command pkg to main to adhere to the lesson naming

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/ramZenit/gator/internal/config"
 )
@@ -25,7 +27,49 @@ func main() {
 		fmt.Println("Error reading the config file:", err)
 		return
 	}
+	appState := state{cfg: &cfg}
 
-	fmt.Printf("main: %v\n", cfg)
+	cmdHandlers := commands{make(map[string]func(*state, command) error)}
 
+	cmdHandlers.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+	if err := cmdHandlers.run(&appState, cmd); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("no <username> target for login command")
+	}
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Access granted to:", username)
+	return nil
+}
+
+// run a given command with the provided state if exists
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.handlers[cmd.name]
+	if !exists {
+		return fmt.Errorf("command not found: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+// registers a new handler function for a command name
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
 }
